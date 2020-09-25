59f8d821088c86401cd4fc1e0a0c5b15103be68c
Cesar <cesarvargasescorcia@gmail.com>
2020-09-25 01:11:32 -0500

    CRUD: Dar formato a las filas del documento pdf

--- src/main/java/com/clienteweb/app/util/ListarClientesPdf.java
+++ src/main/java/com/clienteweb/app/util/ListarClientesPdf.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
-import com.clienteweb.app.entity.Cliente;
 import com.lowagie.text.Document;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
@@ -21,13 +20,16 @@ import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
+import com.clienteweb.app.entity.Cliente;
+
 @Component("/views/clientes/listar")
 public class ListarClientesPdf extends AbstractPdfView{
 
+	 /*
+	-        * Visualizar en pdf: http://localhost:8080/views/clientes/?format=pdf
+	-        * */
+
 	
-	/*
-	 * Visualizar en pdf: http://localhost:8080/views/clientes/?format=pdf
-	 * */
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -35,44 +37,121 @@ public class ListarClientesPdf extends AbstractPdfView{
 		@SuppressWarnings("unchecked")
 		List<Cliente> listadoClientes = (List<Cliente>) model.get("clientes");
 		
-		/*
-		 * dar tamaño de hoja y formato, para el ejemplo es tamaño carta (LETTER) 
-		 * y mostrar horizontal(rotate()) - para aplicar los cambios se aplica la función open()
-		 * */
+		/*Fuentes, tamaños y colores para cada seccion*/
+		Font fuenteTitulo = FontFactory.getFont(FontFactory.HELVETICA_BOLD,20,Color.WHITE);
+		Font fuenteTituloColumnas = FontFactory.getFont(FontFactory.HELVETICA_BOLD ,13,new Color(40,190,138));
+		Font fuenteDataCeldas = FontFactory.getFont(FontFactory.COURIER ,10,Color.BLACK);
+		
+		/*Dar tamaño y formato, para el ejemplo es tamaño carta y formato en horizontal */
 		document.setPageSize(PageSize.LETTER.rotate());
-		document.setMargins(-20, -20, 40, 20);
+		document.setMargins(-20, -20, 30, 20);
 		document.open();
-		/*
-		 * Para más ejemplos de como modificar las celdas en: https://github.com/LibrePDF/OpenPDF/tree/master/pdf-toolbox/src/test/java/com/lowagie/examples
-		 * */
-		PdfPTable tablaTitulo = new PdfPTable(1);
 		PdfPCell celda = null;
 		
-		Font fuenteTitulo = FontFactory.getFont("Helvetica",18,Color.WHITE);
+		/*Tabla Para El Titulo del PDF*/
+		PdfPTable tablaTitulo = new PdfPTable(1);
+						
 		celda = new PdfPCell(new Phrase("LISTADO GENERAL DE CLIENTES", fuenteTitulo));
 		celda.setBorder(0);
 		celda.setBackgroundColor(new Color(40,190,138));
-		celda.setHorizontalAlignment(Element.ALIGN_CENTER);
-		celda.setVerticalAlignment(Element.ALIGN_CENTER);
+		celda.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+		celda.setVerticalAlignment(PdfPCell.ALIGN_CENTER);
 		celda.setPadding(30);
 		
 		tablaTitulo.addCell(celda);
-		/*Dando espacio al titulo del cuerpo de la tabla*/
 		tablaTitulo.setSpacingAfter(30);
 		
-		
+		/*Tabla Para Mostrar Listado de Clientes*/
 		PdfPTable tablaClientes = new PdfPTable(6);
-		listadoClientes.forEach(cliente -> {
-					tablaClientes.addCell(cliente.getId().toString());
-					tablaClientes.addCell(cliente.getNombres());
-					tablaClientes.addCell(cliente.getApellidos());
-					tablaClientes.addCell(cliente.getTelefono());
-					tablaClientes.addCell(cliente.getEmail());
-					tablaClientes.addCell(cliente.getCiudad().getCiudad());
-		});
+		tablaClientes.setWidths(new float[] {0.8f, 2f, 2f, 2f, 3.5f, 1.5f});		
 		
+		celda = new PdfPCell(new Phrase("ID", fuenteTituloColumnas));
+		celda.setBackgroundColor(Color.lightGray);
+		celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+		celda.setVerticalAlignment(Element.ALIGN_CENTER);
+		celda.setPadding(10);
+		tablaClientes.addCell(celda);
+		
+		celda = new PdfPCell(new Phrase("NOMBRES", fuenteTituloColumnas));
+		celda.setBackgroundColor(Color.lightGray);
+		celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+		celda.setVerticalAlignment(Element.ALIGN_CENTER);
+		celda.setPadding(10);
+		tablaClientes.addCell(celda);
+		
+		celda = new PdfPCell(new Phrase("APELLIDOS", fuenteTituloColumnas));
+		celda.setBackgroundColor(Color.lightGray);
+		celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+		celda.setVerticalAlignment(Element.ALIGN_CENTER);
+		celda.setPadding(10);
+		tablaClientes.addCell(celda);
+		
+		celda = new PdfPCell(new Phrase("TELEFONO", fuenteTituloColumnas));
+		celda.setBackgroundColor(Color.lightGray);
+		celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+		celda.setVerticalAlignment(Element.ALIGN_CENTER);
+		celda.setPadding(10);
+		tablaClientes.addCell(celda);
+		
+		celda = new PdfPCell(new Phrase("EMAIL", fuenteTituloColumnas));
+		celda.setBackgroundColor(Color.lightGray);
+		celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+		celda.setVerticalAlignment(Element.ALIGN_CENTER);
+		celda.setPadding(10);
+		tablaClientes.addCell(celda);
+		
+		celda = new PdfPCell(new Phrase("CIUDAD", fuenteTituloColumnas));
+		celda.setBackgroundColor(Color.lightGray);
+		celda.setHorizontalAlignment(Element.ALIGN_CENTER);
+		celda.setVerticalAlignment(Element.ALIGN_CENTER);
+		celda.setPadding(10);
+		tablaClientes.addCell(celda);		
+		
+													
+		/*Bucle For, mostrar todos los datos de los clientes*/		
+		
+		for (Cliente cliente : listadoClientes) {
+			celda = new PdfPCell(new Phrase(cliente.getId().toString(), fuenteDataCeldas));
+			celda.setPadding(5);
+			tablaClientes.addCell(celda);
+			
+			celda = new PdfPCell(new Phrase(cliente.getNombres(), fuenteDataCeldas));
+			celda.setPadding(5);
+			tablaClientes.addCell(celda);
+			
+			celda = new PdfPCell(new Phrase(cliente.getApellidos(), fuenteDataCeldas));
+			celda.setPadding(5);
+			tablaClientes.addCell(celda);
+			
+			celda = new PdfPCell(new Phrase(cliente.getTelefono(), fuenteDataCeldas));
+			celda.setPadding(5);
+			tablaClientes.addCell(celda);
+			
+			celda = new PdfPCell(new Phrase(cliente.getEmail(), fuenteDataCeldas));
+			celda.setPadding(5);
+			tablaClientes.addCell(celda);
+			
+			celda = new PdfPCell(new Phrase(cliente.getCiudad().getCiudad(), fuenteDataCeldas));
+			celda.setPadding(5);
+			tablaClientes.addCell(celda);
+			
+		}
+		
+		/*
+		listadoClientes.forEach(cliente -> {												
+			tablaClientes.addCell(cliente.getId().toString());					
+			tablaClientes.addCell(cliente.getNombres());
+			tablaClientes.addCell(cliente.getApellidos());
+			tablaClientes.addCell(cliente.getTelefono());
+			tablaClientes.addCell(cliente.getEmail());
+			tablaClientes.addCell(cliente.getCiudad().getCiudad());
+		});
+		*/
+		
+		/*Anexamos las Tablas al Documento*/
 		document.add(tablaTitulo);
 		document.add(tablaClientes);
 	}
 
 }
+
